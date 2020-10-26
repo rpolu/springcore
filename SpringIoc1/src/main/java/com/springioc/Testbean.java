3780c96923c43dce2b9014500f5e498d51ae8bc4
rpolu <rpolu@192.168.1.100>
2020-10-26 08:17:26 +0530

    committed

--- SpringIoc1/src/main/java/com/springioc/Testbean.java
+++ SpringIoc1/src/main/java/com/springioc/Testbean.java
@@ -2,29 +2,14 @@ package com.springioc;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 public class Testbean {
-
 	public static void main(String[] args) {
 		Resource resource = new ClassPathResource("config.xml");
 		BeanFactory beanFactory = new XmlBeanFactory(resource);
-
 		Empl empl1 = (Empl) beanFactory.getBean("eb");
-		Empl empl2 = (Empl) beanFactory.getBean("eb");
-
-		System.out.println(empl1);
-		System.out.println(empl2);
-		
-		
-		
-		FileSystemXmlApplicationContext ac;
-		ClassPathXmlApplicationContext ac2;
-		Web
-
+		empl1.displayData();
 	}
-
 }
